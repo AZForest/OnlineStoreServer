1e92a0500716e61d484a66011b702c01245c6fd1
AZForest <61096655+AZForest@users.noreply.github.com>
2021-03-04 09:24:58 -0800

    4th

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ let Product = require('../models/product.model');
 router.route('/').get((req, res) => {
     Product.find()
     .then(products => {
-        res.json(products);
+        res.send(products);
     })
     .catch(err => res.status(400).json('Error is: ' + err));
 });
